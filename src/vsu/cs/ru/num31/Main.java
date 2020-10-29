c82febb2bb8206fe5d0feea56155119d2fa651d6
Dmitry Kulinchenko <dima.kul22@mail.ru>
2020-10-29 12:59:48 +0300

    Task3

--- src/vsu/cs/ru/num31/Main.java
+++ src/vsu/cs/ru/num31/Main.java
@@ -7,6 +7,7 @@ public class Main {
 
         Picture picture = new Picture(new VerticalParabola(5,1,-1), new Line(0,3,0),
                                         new Circle(-6,4,2));
+        
         if (runTests(picture) == false) {
             System.out.println("\n" + "There are errors in program");
             return;
@@ -23,7 +24,7 @@ public class Main {
         SimpleColor[] correctResultsArray = {SimpleColor.GREEN, SimpleColor.ORANGE, SimpleColor.WHITE,
                 SimpleColor.YELLOW, SimpleColor.GREEN, SimpleColor.WHITE};
 
-        Point[] arrayOfCoordinate = {new Point(5, 0.5), new Point(-6, 2.5), new Point(-7, 4),
+        Point[] arrayOfCoordinate = {new Point(5.3, 0.5), new Point(-6, 2.5), new Point(-7, 4),
                 new Point(8, 5), new Point(3, -9), new Point(-6, 5) };
 
         boolean result = true;
@@ -43,8 +44,7 @@ public class Main {
     }
 
     private static void printTest (double x, double y, SimpleColor currentResults, SimpleColor correctResults, String testResult) {
-        System.out.printf("For coordinates (%.1f; %.1f) the result is %s: %s is %s \n",
-                x, y, testResult, currentResults, correctResults);
+        System.out.printf("For coordinates (%.1f; %.1f) the result is %s: %s is %s \n", x, y, testResult, currentResults, correctResults);
     }
 
     private static double readCoordinate(char name){
